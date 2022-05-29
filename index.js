054b9f1d97a2e19af4433933917215aa89cda199
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-29 21:25:20 +0600

    order section

--- index.js
+++ index.js
@@ -102,6 +102,12 @@ async function run() {
       //GET Dynamic (Bookings)
      
 
+      app.get("/bookings/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await bookingsCollection.findOne(query);
+        res.send(result);
+      });
 
 
 
